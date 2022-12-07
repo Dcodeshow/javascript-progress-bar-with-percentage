410893564ab648e7f422fefc9820488ca33e63c9
Dcode Show <dcodeshow@gmail.com>
2022-12-07 18:19:29 +0530

    Progress Bar

--- js/global.js
+++ js/global.js
@@ -1,5 +1,6 @@
 setTimeout(() => {
   let progressBar = document.getElementById("myProgressBar");
+
   let barwidth = 10;
 
   let ab = setInterval(progressFunction, 40);
@@ -7,27 +8,26 @@ setTimeout(() => {
   function progressFunction() {
     if (barwidth >= 100) {
       clearInterval(ab);
-      //step 3
       setTimeout(() => {
         progressBar.classList.add("height-0");
       }, 3000);
-      //step 4
       setTimeout(() => {
         document.getElementById("loader").classList.add("hide-loader");
       }, 4000);
     } else {
       barwidth++;
       progressBar.style.width = barwidth + "%";
-      //step 2
       document.getElementById("incrementpercentage").innerHTML = barwidth + "%";
     }
   }
 }, 1000);
 
-/* Prerequisite
-1 setInterval
-2 setTimeout
-3 classlist method
-4 Increment operator
+/*
+Prerequisite
+1 setinterval
+2 settimeout
+3 classlist add method
+4 increment operator
+5 if else condition
 
 */
